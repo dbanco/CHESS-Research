439046858edbc61753e4d8e02a915538cb0151b5
dbanco <daniel.banco@tufts.edu>
2018-02-08 19:26:31 -0500

    slight change to init

--- MATLAB/init_FISTA_Circulant.m
+++ MATLAB/init_FISTA_Circulant.m
@@ -16,12 +16,11 @@ A0ft_stack = unshifted_basis_matrix_ft_stack(P);
 [m,n,t,r] = size(A0ft_stack);
 x_init = ones(m,n,t,r);
 
-%% Run FISTA
+% Run FISTA
 [x_hat, err, obj, l_0] = FISTA_Circulant(A0ft_stack,polar_image,x_init,P.params);
 
-%% Save outputs
+% Save outputs
 save(fullfile(output_dir,sprintf('spatial_fit_%i_%i.mat',P.load_step,P.img)),'x_hat','err','polar_image','P')
-
 
 end
 
