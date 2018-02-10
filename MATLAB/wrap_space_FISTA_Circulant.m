5b0a50a9e17b41ed738a05c88fb4096f9163dcec
dbanco <daniel.banco@tufts.edu>
2018-02-09 22:44:46 -0500

    Fixed lack of input args to space FISTA

--- MATLAB/wrap_space_FISTA_Circulant.m
+++ MATLAB/wrap_space_FISTA_Circulant.m
@@ -12,7 +12,7 @@ A0ft_stack = unshifted_basis_matrix_ft_stack(P);
 
 %% Run FISTA updating solution and error array
 [x_neighbors,vdfs] = load_neighbors_vdf(fullfile(output_dir,baseFileName),P);
-[x_hat, err_new, ~, ~] = space_FISTA_Circulant(A0ft_stack,polar_image,x_hat,P.params);
+[x_hat, err_new, ~, ~] = space_FISTA_Circulant(A0ft_stack,polar_image,x_neighbors,vdfs,x_hat,P.params);
 err = [err;err_new];
 
 %% Save outputs, updating the coefficients of the previous iteration
