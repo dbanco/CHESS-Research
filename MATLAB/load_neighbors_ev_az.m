ede4ec5470abef890bf7065c776dbefb995e3ed2
dbanco <daniel.banco@tufts.edu>
2018-04-09 16:05:03 -0400

    Expected variance regularization.

---
+++ MATLAB/load_neighbors_ev_az.m
@@ -0,0 +1,41 @@
+function [neighbors, evs] = load_neighbors_ev_az(baseFileName,P)
+%neighbors Returns term used in gradient of vdf objective and vdfs 
+%          of coefficients neighboring point (i,j)
+
+% Get list of possible neighbors
+row = floor(P.img/5)+1;
+col = mod(P.img,5)+1;
+rows = [row-1;
+        row+1; 
+        row;
+        row];
+cols = [col;
+        col; 
+        col-1;
+        col+1];
+
+% Remove invalid neighbors
+keep = (rows >= 1).*(rows <=P.sampleDims(1)).*...
+       (cols >= 1).*(cols <=P.sampleDims(2));
+neighbor_imgs = [rows(keep==1),cols(keep==1)];
+
+%Get contributions and vdfs from neighbors
+n = P.num_rad;
+m = P.num_theta;
+T = P.num_var_t;
+R = P.num_var_r;
+neighbors = zeros(n,m,T,R);
+vdfs = {}; 
+for i = 1:size(neighbor_imgs,1)
+    n_img = sub2ind(flip(P.sampleDims),neighbor_imgs(i,2),neighbor_imgs(i,1));
+    load(sprintf(baseFileName,P.load_step,n_img-1))
+    x_hat_var = x_hat;
+    ev = compute_exp_az_variance(x_hat_var,P.var_theta)
+    for t = 1:T
+        for r = 1:R
+            neighbors(:,:,t,r) = neighbors(:,:,t,r) + ones(n,m)*vdf(t,r);
+        end
+    end
+end
+
+
