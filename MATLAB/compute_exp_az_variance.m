97a0ae1a1c611c311615d1e75050122661bd5020
dbanco <daniel.banco@tufts.edu>
2018-04-11 00:43:51 -0400

    fix2

--- MATLAB/compute_exp_az_variance.m
+++ MATLAB/compute_exp_az_variance.m
@@ -2,5 +2,5 @@ function evar = compute_exp_az_variance(x,var_theta)
 az_signal = squeeze(sum(sum(sum(x,1),2),4));
 total = sum(az_signal);
 evar = var_theta.*az_signal./total;
-evar = sum(evar);
+evar = sum(evar(:));
 end
