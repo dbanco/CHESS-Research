5b9df27504d26a225a63da94cb56d09606e79ba1
dbanco <daniel.banco@tufts.edu>
2018-04-10 21:54:14 -0400

    That function was messed up/

--- MATLAB/compute_exp_az_variance.m
+++ MATLAB/compute_exp_az_variance.m
@@ -1,15 +1,6 @@
-function evar = compute_exp_az_variance(var_signal,var_theta)
-az_var_signal = squeeze(sum(var_signal,2));
-[~, s2, s3, s4] = size(az_var_signal);
-evar = zeros(s2,s3,s4);
-for j = 1:s2
-    for k = 1:s3
-        for l = 1:s4
-            total = sum(az_var_signal(:,j,k,l));
-            for i = 1:numel(var_theta)
-                evar(j,k,l) = evar(j,k,l) +...
-                      var_theta(i)*az_var_signal(i,j,k,l)/total;
-            end
-        end        
-    end
+function evar = compute_exp_az_variance(x,var_theta)
+az_signal = squeeze(sum(sum(sum(x,1),2),4));
+total = sum(az_signal);
+evar = sum(var_theta.*az_signal/total);
+
 end
