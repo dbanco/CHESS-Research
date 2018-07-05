85d871b994c6fd361b0ca1947df70e4be7d7d924
dbanco <daniel.banco@tufts.edu>
2018-07-05 12:59:38 -0400

    adapt to image size in wrapper func.

--- MATLAB/wrap_mmpad_norm2_FISTA_Circulant.m
+++ MATLAB/wrap_mmpad_norm2_FISTA_Circulant.m
@@ -1,16 +1,19 @@
 function wrap_mmpad_norm2_FISTA_Circulant(datadir,P,outputdir)
 %wrap_FISTA_Circulant 
 
-%% Generate unshifted basis function matrices
-A0ft_stack = unshifted_basis_matrix_ft_stack_norm2(P);
-
 %% load polar image
 str2 = sprintf('%i',P.img);
 fileName = ['mmpad_img_',str2,'.mat'];
 fileDir = fullfile(datadir,fileName);
 load(fileDir)
 
-% Initialize solution
+P.num_rad = size(polar_image,1);
+P.num_theta = size(polar_image,2);
+
+%% Generate unshifted basis function matrices
+A0ft_stack = unshifted_basis_matrix_ft_stack_norm2(P);
+
+%% Initialize solution
 [m,n,t,r] = size(A0ft_stack);
 x_init = ones(m,n,t,r);
 
