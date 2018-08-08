d98069d6c0bd10500770475e6d8fd14e95647fa4
dbanco <daniel.banco@tufts.edu>
2018-08-07 22:20:57 -0400

    file name of data in slurm_batch script

--- MATLAB/slurm_batch_inputs_mmpad_reg.m
+++ MATLAB/slurm_batch_inputs_mmpad_reg.m
@@ -6,7 +6,7 @@ dataset = fullfile(datadir,'mmpad_polar');
 ringName = 'ring1';
 
 % Output directory
-outputdir = fullfile('/cluster','shared','dbanco02',['mmpad_',ringName,'_fit']);
+outputdir = fullfile('/cluster','shared','dbanco02',['mmpad_',ringName,'_fit_reg1']);
 mkdir(outputdir)
 
 % Function
@@ -35,7 +35,7 @@ P.var_rad   = linspace(P.drad/2,  32,P.num_var_r).^2;
 % Zero padding and mask
 maskRows = 129:133;
 zPad = [5,0];
-
+load(fullfile(dataset,ringName,'mmpad_img_0.mat'));
 zMask = zeros(size(polar_image));
 zMask(maskRows,:) = 1;
 zMask = onePad(zMask,zPad);
