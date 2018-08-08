cf06771833cf84a23b4710c5acfc06601b55b7da
dbanco <daniel.banco@tufts.edu>
2018-08-07 22:26:06 -0400

    error

--- MATLAB/slurm_batch_inputs_mmpad_reg.m
+++ MATLAB/slurm_batch_inputs_mmpad_reg.m
@@ -35,7 +35,7 @@ P.var_rad   = linspace(P.drad/2,  32,P.num_var_r).^2;
 % Zero padding and mask
 maskRows = 129:133;
 zPad = [0,0];
-load(fullfile(dataset,ringName,'mmpad_img_0.mat'));
+load(fullfile(dataset,ringName,'mmpad_img_1.mat'));
 zMask = zeros(size(polar_image));
 zMask(maskRows,:) = 1;
 zMask = onePad(zMask,zPad);
