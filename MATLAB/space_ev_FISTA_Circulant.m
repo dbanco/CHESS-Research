a5dc6d75a15f9d31874ca534fc2fb60c650546b3
dbanco <daniel.banco@tufts.edu>
2018-09-17 13:19:01 -0400

    Renaming

--- MATLAB/space_ev_FISTA_Circulant.m
+++ MATLAB/space_ev_FISTA_Circulant.m
@@ -1,4 +1,4 @@
-function [x_hat, err, obj, l_0] = space_ev_FISTA_Circulant(A0ft_stack,b,neighbors_ev_az_neighbors_ev_rad,var_theta,var_rad,x_init,params)
+function [x_hat, err, obj, l_0] = space_ev_FISTA_Circulant(A0ft_stack,b,neighbors_ev_az,neighbors_ev_rad,var_theta,var_rad,x_init,params)
 %FISTA_Circulant Image regression by solving LASSO problem 
 %                argmin_x ||Ax-b||^2 + lambda||x|| +...
 %                         gamma sum_{adjacent_xi}^4 (1/4)||xn-x||^2
