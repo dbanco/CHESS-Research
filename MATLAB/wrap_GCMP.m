a6dbc2f2d007fc4d67a080e197025d7c62b9cfb1
dbanco <daniel.banco@tufts.edu>
2019-01-16 11:44:18 -0500

    zeroPad error

--- MATLAB/wrap_GCMP.m
+++ MATLAB/wrap_GCMP.m
@@ -14,7 +14,8 @@ catch
 end
 
 %% Zero pad image
-b = zeroPad(polar_image,P.params.zeroPad);
+% b = zeroPad(polar_image,P.params.zeroPad);
+b = polar_image
 
 P.num_rad = size(b,1);
 P.num_theta = size(b,2);
