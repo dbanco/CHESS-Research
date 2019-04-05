ba110550848e2738443f1b2fb81690b8165d29ee
dbanco <dbanco02@xfer001.lux.tufts.edu>
2019-04-05 15:09:07 -0400

    cluster-side new file

---
+++ MATLAB/parallel_FISTA_script.m
@@ -0,0 +1,19 @@
+function parallel_reg_wass_FISTA_script(n)
+parpool(32)
+% Initialize unregularized solution
+%parfor idx = 1:n
+%    slurm_batch_wrapper(idx);
+%end
+
+% 10 pairs of regularized iterations
+for i = 1:10
+    cd '../job_wass_parallel_ab'
+    parfor idx = 1:n
+        slurm_batch_wrapper(idx);
+    end
+    
+    cd '../job_wass_parallel_ba'
+    parfor idx = 1:n
+        slurm_batch_wrapper(idx);
+    end
+end
