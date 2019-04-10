d0d35fed824608a094cf25758cb30b9a1640f4ef
dbanco <daniel.banco@tufts.edu>
2019-04-10 16:28:32 -0400

    Fix parallel script

--- MATLAB/parallel_FISTA_script.m
+++ MATLAB/parallel_FISTA_script.m
@@ -1,19 +1,7 @@
-function parallel_reg_wass_FISTA_script(n)
+function parallel_FISTA_script(n)
 parpool(32)
 % Initialize unregularized solution
-%parfor idx = 1:n
-%    slurm_batch_wrapper(idx);
-%end
-
-% 10 pairs of regularized iterations
-for i = 1:10
-    cd '../job_wass_parallel_ab'
-    parfor idx = 1:n
-        slurm_batch_wrapper(idx);
-    end
-    
-    cd '../job_wass_parallel_ba'
-    parfor idx = 1:n
-        slurm_batch_wrapper(idx);
-    end
+parfor idx = 1:n
+   slurm_batch_wrapper(idx);
 end
+
