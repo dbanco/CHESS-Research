5f072f8ca3081f5b35990bdf9cb0bd7330a9edf8
dbanco <dbanco02@xfer001.lux.tufts.edu>
2019-04-29 16:22:18 -0400

    cluster change

--- MATLAB/parallel_reg_wass_FISTA.m
+++ MATLAB/parallel_reg_wass_FISTA.m
@@ -7,12 +7,12 @@ end
 
 % 10 pairs of regularized iterations
 for i = 1:20
-    cd '../job_wass_parallel_ab_10'
+    cd '../job_wass_parallel_ab'
     parfor idx = 1:n
         slurm_batch_wrapper(idx);
     end
     
-    cd '../job_wass_parallel_ba_10'
+    cd '../job_wass_parallel_ba'
     parfor idx = 1:n
         slurm_batch_wrapper(idx);
     end
