c45463c0e805394c0ab8ceda24c2d2d0ca1dac0f
dbanco <daniel.banco@tufts.edu>
2019-04-29 16:25:11 -0400

    new file

---
+++ MATLAB/parallel_reg_wass_FISTA_10.m
@@ -0,0 +1,19 @@
+function parallel_reg_wass_FISTA_10(n)
+parpool(32)
+% Initialize unregularized solution
+parfor idx = 1:n
+   slurm_batch_wrapper(idx);
+end
+
+% 10 pairs of regularized iterations
+for i = 1:20
+    cd '../job_wass_parallel_ab_10'
+    parfor idx = 1:n
+        slurm_batch_wrapper(idx);
+    end
+    
+    cd '../job_wass_parallel_ba_10'
+    parfor idx = 1:n
+        slurm_batch_wrapper(idx);
+    end
+end
