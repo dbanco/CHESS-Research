b32197f154d458ad2b40e4f1149c3739a6af8028
dbanco <daniel.banco@tufts.edu>
2019-07-24 11:29:03 -0400

    two_spot_growth 0.2 param

--- MATLAB/sequential_cyclic_two_spot_growth_parallel.m
+++ MATLAB/sequential_cyclic_two_spot_growth_parallel.m
@@ -2,8 +2,8 @@ P.set = 1;
 P.img = 1;
 
 dataset = '/cluster/home/dbanco02/simulated_data_two_spot_growth/';
-output_dirA = '/cluster/shared/dbanco02/unreg_two_spot_growth_a';
-output_dirB = '/cluster/shared/dbanco02/unreg_two_spot_growth_b';
+output_dirA = '/cluster/shared/dbanco02/two_spot_growth_2a';
+output_dirB = '/cluster/shared/dbanco02/two_spot_growth_2b';
 mkdir(output_dirA)
 mkdir(output_dirB)
 prefix = 'polar_image';
@@ -35,7 +35,7 @@ params.L = 1000;
 params.t_k = 1;
 params.lambda = 0.0359;
 params.wLam = 25;
-params.gamma = 0.5;
+params.gamma = 0.2;
 params.beta = 1.2;
 params.maxIter = 800;
 params.maxIterReg = 800;
