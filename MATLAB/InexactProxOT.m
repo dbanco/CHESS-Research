51f81ee4758ecdc72539ee0297ee305da6b4baab
dbanco <daniel.banco@tufts.edu>
2019-07-24 16:37:27 -0400

    3rd attempt.

--- MATLAB/InexactProxOT.m
+++ MATLAB/InexactProxOT.m
@@ -1,4 +1,4 @@
-function [Wd, r, c, Gamma] = InexactProxOT(r, c, D, beta, L)
+function [Wd, r, c, Gamma, a_t] = InexactProxOT(r, c, D, beta, L)
 %sinkhornKnoppTransport
 
 % Remove 0 probability points from each distribution, normalize
@@ -15,18 +15,19 @@ G = exp(-D/beta);
 Gamma = ones(size(D));
 
 b = ones(size(r))/numel(r);
-
-for i = 1:100
+a_t = {};
+for i = 1:1000
     Q = G.*Gamma;
     for t = 1:L
        a = r./(Q*b);
        b = c./(Q'*a);
     end
+    a_t{i} = a;
     Gamma_new = diag(a)*Q*diag(b);
-%     check = norm(Gamma_new-Gamma);
-%     if sum(abs(check ))<1e-8
-%         break
-%     end
+    check = norm(Gamma_new-Gamma);
+    if sum(abs(check ))<1e-8
+        break
+    end
     Gamma = Gamma_new;
 end
 
