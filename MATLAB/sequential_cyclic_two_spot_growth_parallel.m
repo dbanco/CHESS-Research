b566a04f3a471d31aec5511a43e493b6def55661
dbanco <daniel.banco@tufts.edu>
2019-07-30 17:15:18 -0400

    Full regularized runs.

--- MATLAB/sequential_cyclic_two_spot_growth_parallel.m
+++ MATLAB/sequential_cyclic_two_spot_growth_parallel.m
@@ -2,8 +2,8 @@ P.set = 1;
 P.img = 1;
 
 dataset = '/cluster/home/dbanco02/simulated_data_two_spot_growth/';
-output_dirA = '/cluster/shared/dbanco02/two_spot_growth_3a';
-output_dirB = '/cluster/shared/dbanco02/two_spot_growth_3b';
+output_dirA = '/cluster/shared/dbanco02/two_spot_growth_6a';
+output_dirB = '/cluster/shared/dbanco02/two_spot_growth_6b';
 mkdir(output_dirA)
 mkdir(output_dirB)
 prefix = 'polar_image';
@@ -35,7 +35,7 @@ params.L = 1000;
 params.t_k = 1;
 params.lambda = 0.0359;
 params.wLam = 25;
-params.gamma = 0.1;
+params.gamma = 0.05;
 params.beta = 1.2;
 params.maxIter = 800;
 params.maxIterReg = 800;
