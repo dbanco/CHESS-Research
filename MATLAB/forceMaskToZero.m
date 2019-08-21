2fcb30f6aa198c7adb7a7efbb4801719867bf0d8
dbanco <daniel.banco@tufts.edu>
2019-08-21 04:24:44 -0400

    independent two spot far data

--- MATLAB/forceMaskToZero.m
+++ MATLAB/forceMaskToZero.m
@@ -1,8 +1,13 @@
 function x = forceMaskToZero( x, zeroMask )
 %forceMaskToZero Forces zeroMask region to 0
-
-if(max(size(zeroMask))>0)
-    x(zeroMask(:,1),zeroMask(:,2)) = 0;
+try
+    if(max(size(zeroMask))>0)
+        x(zeroMask(:,1),zeroMask(:,2)) = 0;
+    end
+catch
+    if(max(size(zeroMask))>0)
+        x(zeroMask(:,2)) = 0;
+    end 
 end
 
 end
