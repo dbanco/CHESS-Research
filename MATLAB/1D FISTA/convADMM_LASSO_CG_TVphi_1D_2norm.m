0eb85693e01e75243bafc4cea1f542ccea212d70
daniel <dpqb10@gmail.com>
2021-02-17 09:15:26 -0500

    2norm routine

---
+++ MATLAB/1D FISTA/convADMM_LASSO_CG_TVphi_1D_2norm.m
@@ -0,0 +1,186 @@
+function [X_hat, err, obj, l1_norm, tv_penalty] = convADMM_LASSO_CG_TVphi_1D_2norm(A0ft_stack,B,X_init,params)
+%convADMM_LASSO_1D Image regression by solving LASSO problem 
+%                argmin_x 0.5*||Ax-b||^2 + lambda||x||_1
+%
+% Inputs:
+%   b          - (n) polar ring image
+%   A0ft_stack - (n x t) fft of unshifted gaussian basis matrices
+%   params     - struct containing the following field
+%   lambda     - l1 penalty parameter > 0
+%   lambda2    - TVx penalty parameter > 0
+%   adaptRho   - adaptive rho enable: 1 or 0
+%   rho        - admm penalty parameter > 0
+%   rho2       - admm penalty parameter > 0
+%   tau        - adaptive rho parameter: 1.01-1.5
+%   mu         - separation factor between primal and dual residual
+%   alpha      - momentum parameter1.1-1.8
+%   isNonnegative - flag to enforce nonnegative solution
+%   x_init - initial guess of solution
+%
+%   stoppingCriterion - 'OBJECTIVE_VALUE' or 'COEF_CHANGE'
+%   tolerance - tolerance for stopping criterion
+%   maxIter - maximum number of iterations
+%
+%   zeroPad         - [row_pad_width,col_pad_width]
+%   zeroMask        - [row_indices,col_indices]
+%   plotProgress    - 0 or 1
+%
+% Outputs:
+% x_hat - (n x t) solution 
+% err - (nIters) relative error of solution at each iteration
+% obj - (nIters) objective value of solution at each iteration
+% l_0 - (nIters) sparsity of solution at each iteration
+%
+
+% Get parameters
+tolerance = params.tolerance;
+lambda1 = params.lambda1;
+lambda2 = params.lambda2;
+rho1 = params.rho1;
+rho2 = params.rho2;
+mu = params.mu;
+adaptRho = params.adaptRho;
+tau = params.tau;
+alpha = params.alpha;
+maxIter = params.maxIter;
+isNonnegative = params.isNonnegative;
+
+zPad = params.zeroPad;
+zMask = params.zeroMask;
+
+[N,K,T] = size(X_init);
+BnormSq = sum(B.^2, 1);
+
+% Initialize variables
+X_init = forceMaskToZeroArray(X_init,zMask);
+Xk = X_init;
+
+% L1 norm variable/lagranage multipliers
+Yk = zeros(N,K,T);
+Vk = zeros(N,K,T);
+
+% Track error and objective
+err = nan(1,maxIter);
+l1_norm = nan(1,maxIter);
+tv_penalty = nan(1,maxIter);
+obj = nan(1,maxIter);
+
+keep_going = 1;
+nIter = 0;
+count = 0;
+while keep_going && (nIter < maxIter)
+    nIter = nIter + 1;   
+    
+    % x-update
+    [Xkp1,cgIters] = conjGrad_TVphi_1D( A0ft_stack,B,Xk,(Yk-Vk),(Zk-Uk),params);
+    
+    % y-update and v-update
+    for t = 1:T
+        Ykp1(:,:,t) = soft(alpha*Xkp1(:,:,t) + (1-alpha)*Yk(:,:,t) + Vk(:,:,t), lambda1(t)/rho1);
+    end
+    if isNonnegative
+        Ykp1(Ykp1<0) = 0;
+    end
+    Vk = Vk + alpha*Xkp1 + (1-alpha)*Yk - Ykp1;
+    
+    % Track and display error, objective, sparsity
+    fit = Ax_ft_1D_Time(A0ft_stack,Xkp1);
+    err(nIter) = sum(((B-fit).^2)./BnormSq,'all');
+    Xsum = 0;
+    for t = 1:T
+        Xsum = Xsum + lambda1(t)*sum(abs(Xkp1(:,:,t)),'all');
+    end
+    l1_norm(nIter) = Xsum;
+    tv_penalty(nIter) = lambda2*sum(abs(DiffPhiX_1D(Xkp1)),'all');
+    f = 0.5*err(nIter) + l1_norm(nIter) + tv_penalty(nIter);
+    
+    obj(nIter) = f;
+    if params.verbose
+        disp(['Iter ',     num2str(nIter),...
+              ' cgIters ',  num2str(cgIters),...
+              ' Rho1 ',     num2str(rho1),...
+              ' Obj ',     num2str(obj(nIter)),...
+              ' Err ',     num2str(0.5*err(nIter)),...
+              ' ||x||_1 ', num2str(l1_norm(nIter)),...
+              ' TVx ',     num2str(tv_penalty(nIter)),...
+              ' ||x||_0 ', num2str(sum(Xkp1(:) >0))
+               ]);
+    end
+    
+    if params.plotProgress
+        figure(1)    
+        hold off
+        plot(b)
+        hold on
+        plot(fit)
+        legend('data','fit')
+        
+        pause
+    end
+    
+    % Check stopping criterion
+    switch params.stoppingCriterion
+        case 'OBJECTIVE_VALUE'
+            % compute the stopping criterion based on the relative
+            % variation of the objective function.
+            try
+                criterionObjective = abs(obj(nIter)-obj(nIter-1));
+                keep_going =  (criterionObjective > tolerance);
+            catch
+                keep_going = 1;
+            end
+        case 'COEF_CHANGE'
+            diff_x = sum(abs(Xkp1(:)-Xk(:)))/numel(xk);
+            keep_going = (diff_x > tolerance);
+        otherwise
+            error('Undefined stopping criterion.');
+    end
+    
+    if adaptRho
+        skY = rho1*sum((Ykp1(:)-Yk(:)).^2);
+        rkY = sum((Xkp1(:)-Ykp1(:)).^2);
+        if sqrt(rkY) > mu*sqrt(skY)
+            rho1 = rho1*tau;
+        elseif sqrt(skY) > mu*sqrt(rkY)
+            rho1 = rho1/tau;
+        end
+    end
+
+    % Stop if objective increases consistently
+    if (nIter > 10) && (obj(nIter-1) < obj(nIter))
+        count = count + 1;
+        if count > 20
+            keep_going = 0;
+        end
+    else
+        count = 0;
+    end
+
+%     if obj(nIter) > 1e10
+%         keep_going = 0;
+%     end
+
+    % Update indices
+    Xk = Xkp1;
+    Yk = Ykp1;
+    if obj(nIter) <= min(obj)
+        Xmin = Xkp1;
+    end
+    
+end
+
+X_hat = Xmin;
+if isNonnegative
+    X_hat(X_hat<0) = 0;
+end
+
+err = err(1:nIter) ;
+obj = obj(1:nIter) ;
+
+function y = soft(x,T)
+if sum(abs(T(:)))==0
+    y = x;
+else
+    y = max(abs(x) - T, 0);
+    y = sign(x).*y;
+end
---
+++ MATLAB/1D FISTA/slurm_batch_inputs_anomaly_convADMM_LASSO_CG_TVphi_1D_2norm.m
@@ -0,0 +1,134 @@
+% Parameter selection
+disp('Setup params')
+
+% Parent directory
+% top_dir = 'E:\PureTiRD_nr2_c_x39858';
+% top_dir = 'E:\MMPAD_data';
+top_dir = '/cluster/shared/dbanco02';
+noise_added = 0:0.03:0.30;
+noise_thresh = [0.01,0.03:0.03:0.24,0.3,0.5];
+
+for iii = 1:11
+% Input dirs
+dset_name = ['simulated_two_spot_1D_anomaly_small_',num2str(iii)];
+
+% Indep dirs
+indep_name = '_indep_ISM1';
+indep_subdir = [dset_name,indep_name];
+indep_dir = fullfile(top_dir,indep_subdir);
+
+% Output dirs
+output_name = '_coupled_CG_TVphi2';
+output_subdir = [dset_name,output_name];
+
+% Setup directories
+dataset =  fullfile(top_dir,dset_name);
+output_dir  = fullfile(top_dir,output_subdir);
+mkdir(output_dir)  
+
+% File Parameters
+baseFileName = 'indep_fit_%i_%i.mat';
+load(fullfile(indep_dir,sprintf(baseFileName,1,1)));
+P.baseFileName = 'coupled_fit_%i.mat';
+
+N = P.num_theta;
+K = P.num_var_t;
+T = P.num_ims;
+
+% Function
+funcName = 'wrap_convADMM_LASSO_CG_TVphi_1D_2norm';
+
+%% Fixed Parameters
+% Zero padding and mask
+zPad = [0,0];
+zMask = [];
+
+% Construct dictionary
+A0ft_stack = unshifted_basis_vector_ft_stack_zpad(P);
+
+% Algorithm parameters
+P.params.rho2 = 0.005;
+P.params.lambda2 = 1;
+P.params.tau = 1.1;
+P.params.mu = 2;
+P.params.adaptRho = 1;
+P.params.alpha = 1.8;
+P.params.stoppingCriterion = 'OBJECTIVE_VALUE';
+P.params.maxIter = 800;
+P.params.conjGradIter = 50;
+P.params.tolerance = 1e-8;
+P.params.cgEpsilon = 1e-1;
+P.params.isNonnegative = 1;
+P.params.zeroPad = zPad;
+P.params.zeroMask = zMask;
+P.params.plotProgress = 0;
+P.params.verbose = 1;
+
+% Lambda1 values: Use L-curve parameter selection
+indep_data = load(fullfile(indep_dir,sprintf(baseFileName,1,1)));
+lambda1_vals = indep_data.P.lambda_values;
+M_lam1 = numel(lambda1_vals);
+err_select = zeros(M_lam1,T);
+l1_select = zeros(M_lam1,T);
+
+for m = 1:M_lam1
+    for t = 1:T
+        load(fullfile(dataset,[P.prefix,'_',num2str(t),'.mat']))
+        b = P.dataScale*polar_vector(1:179);
+        x_data = load(fullfile(indep_dir,sprintf(baseFileName,m,t)),'x_hat');
+        fit = forceMaskToZero(Ax_ft_1D(A0ft_stack,x_data.x_hat),129:133);
+        err_select(m,t) = sum((fit(:)-b(:)).^2);
+        l1_select(m,t) = sum(x_data.x_hat(:));
+    end
+end
+
+%% L curve parameter selection for l1-norm term
+select_indices = zeros(T,1);
+for t = 1:T
+    err_t = err_select(:,t);
+    l1_t = l1_select(:,t);
+    err_t = err_t;%/max(err_t);
+    l1_t = l1_t;%/max(l1_t);
+    sq_origin_dist = abs(l1_t) + abs(err_t);
+    select_indices(t) = find( sq_origin_dist == min(sq_origin_dist + (err_t == 0) )  );
+end
+
+for t = 1:T
+    load(fullfile(dataset,[P.prefix,'_',num2str(t),'.mat']))
+    b = P.dataScale*polar_vector(1:179);
+    rel_err_t = err_select(:,t)/sum(b(:).^2);
+    while rel_err_t(select_indices(t)) > noise_thresh(data_num)
+        if select_indices(t) > 1
+            select_indices(t) = select_indices(t) - 1;
+        else
+            select_indices(t) = find(rel_err_t == min(rel_err_t));
+            break
+        end
+    end
+end
+
+P.params.lambda1 = lambda1_vals(select_indices);
+P.params.lambda1_indices = select_indices;
+
+% Lambda2 values
+M = 30;
+lambda2_vals = logspace(-4,1,M);
+M = numel(lambda2_vals);
+P.lambda2_values = lambda2_vals;
+
+%% Parameters to vary
+
+% Job directory
+jobDir = fullfile('/cluster','home','dbanco02',['job_',output_subdir]);
+mkdir(jobDir)
+
+for k = 1:M
+    P.params.lambda2 = lambda2_vals(k);
+    P.set = k;
+    varin = {dataset,P,output_dir};
+    save(fullfile(jobDir,['varin_',num2str(k),'.mat']),'varin','funcName')
+end
+
+slurm_write_bash(k-1,jobDir,'full_batch_script.sh',['1-',num2str(M)])
+% slurm_write_matlab(k-1,jobDir,'parallel_FISTA','matlab_batch_script.sh')
+end
