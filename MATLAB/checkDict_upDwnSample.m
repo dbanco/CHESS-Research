3e37e16e2cfda42df06f85c5367c902ae1ef1d19
daniel <dpqb10@gmail.com>
2022-09-08 11:45:34 -0700

    moving files

--- MATLAB/checkDict_upDwnSample.m
+++
@@ -1,9 +0,0 @@
-function f = checkDict_upDwnSample(X,D,U,f)
-K = size(D,3);
-AD0 = upDwnSample(D,U);
-if nargin < 4
-    f = plotDictUsage(AD0,K,X);
-else  
-    f = plotDictUsage(AD0,K,X,f);
-end
-
--- MATLAB/dSample.m
+++
@@ -1,9 +0,0 @@
-function dd = dSample(d,m)
-%decimatedDictionary Summary of this function goes here
-%   Detailed explanation goes here
-
-d1 = lowpassM(d,m,1);
-dd = d1(1:m:end);
-
-end
-
