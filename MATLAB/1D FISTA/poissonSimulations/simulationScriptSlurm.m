c7a0fa819eedfeecd2251112db0e5a6cbe49f62b
daniel <dpqb10@gmail.com>
2022-09-27 22:06:56 -0700

    fix

--- MATLAB/1D FISTA/poissonSimulations/simulationScriptSlurm.m
+++ MATLAB/1D FISTA/poissonSimulations/simulationScriptSlurm.m
@@ -3,13 +3,13 @@ close all
 
 % Parent directory
 % top_dir = 'E:\PureTiRD_nr2_c_x39858';
-top_dir = 'D:\Simulations';
-% top_dir = '/cluster/shared/dbanco02/Simulations';
+% top_dir = 'D:\Simulations';
+top_dir = '/cluster/shared/dbanco02/Simulations';
 mkdir(top_dir)
 
 % Simulation name
 sim = 'linear';
-sim_name = [sim,'PoissonNoise4'];
+sim_name = [sim,'PoissonNoise5'];
 file_name = 'lineSearchNoise';
 
 % Define poisson dataset
@@ -18,7 +18,6 @@ T = 30;
 [P,K,M,MM] = definePoissonP(N,T);
 snr_levels = [10,5,2.5,2,1.75,1.5,1.25,1.1];
 [alpha_vals,theta_stds] = genPoissonScales2(N,T,snr_levels,sim);
-P.theta_stds = theta_stds;
 P.alpha_vals = alpha_vals;
 % [Bn,B,theta_stds,rel_err] = genSimDataPoisson2(N,T,alpha_vals(4),sim);
 
@@ -27,8 +26,8 @@ P.alpha_vals = alpha_vals;
 % Independent
 alg_name = 'IndepISM';
 indep_dir  = fullfile(top_dir,sim_name,alg_name);
-% SimParamSearchPoisson2(P,M,snr_levels,alpha_vals,...
-%                                 indep_dir,file_name,sim)
+SimParamSearchPoisson2(P,M,snr_levels,alpha_vals,...
+                       indep_dir,file_name,sim)
 
 % Coupled
 alg_name = 'CoupledCGTV';
