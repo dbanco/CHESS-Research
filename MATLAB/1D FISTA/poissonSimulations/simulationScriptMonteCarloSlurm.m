8dd81d10f2236d847a5e5776272d7c30ed127ee0
dbanco <dbanco02@login-prod-02.pax.tufts.edu>
2022-09-29 21:09:08 -0400

    cluster updates

--- MATLAB/1D FISTA/poissonSimulations/simulationScriptMonteCarloSlurm.m
+++ MATLAB/1D FISTA/poissonSimulations/simulationScriptMonteCarloSlurm.m
@@ -18,7 +18,7 @@ load(fullfile(top_dir,sim_name,'IndepISM',[ps_name,'_1']));
 % Define poisson dataset
 [N,T] = size(B);
 snr_levels = [10,5,2.5,2,1.75,1.5,1.25,1.1];
-P.trials = 3;
+P.trials = 100;
 alpha_vals = P.alpha_vals;
 theta_stds = P.theta_stds;
 
@@ -27,8 +27,8 @@ theta_stds = P.theta_stds;
 % Independent
 alg_name = 'IndepISM';
 indepPS_dir  = fullfile(top_dir,sim_name,alg_name);
-% SimMCPoisson2(P,snr_levels,alpha_vals,...
-%               indepPS_dir,ps_name,mc_dir,sim)
+SimMCPoisson2(P,snr_levels,alpha_vals,...
+              indepPS_dir,ps_name,mc_dir,sim)
 
 % Coupled
 alg_name = 'CoupledCGTV';
