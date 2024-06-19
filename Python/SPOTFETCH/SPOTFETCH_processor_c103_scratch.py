a4763dce0931e0c30964f9f62a9503f2bb47be07
Daniel Banco <dpqb10@gmail.com>
2024-06-19 14:36:57 -0400

    Chess-side changes

--- Python/SPOTFETCH/SPOTFETCH_processor_c103_scratch.py
+++ Python/SPOTFETCH/SPOTFETCH_processor_c103_scratch.py
@@ -13,43 +13,48 @@ import shutil
 # %% Processing Setup
 
 # Output data path
-topPath = "/nfs/chess/user/dbanco/c103_processing/Sample-1/layer-1"
-# topPath = "/mnt/scratch/dbanco/c103_processing/Sample-1/layer-1"
+topPath_nfs = "/nfs/chess/user/dbanco/c103_processing/Sample-1/layer-1"
+topPath_mnt = "/mnt/scratch/dbanco/c103_processing/Sample-1/layer-1"
 
 # Raw HEXD Data
 exsituPath = "/nfs/chess/raw/2024-2/id3a/miller-3528-c/c103-1-ff-1/1/ff"
 dataPath = "/mnt/scratch/....TBD"
+initTracksPath_nfs = os.path.join(topPath_nfs,'initTracks')
+initTracksPath_mnt = os.path.join(topPath_mnt,'initTracks')
 
 # Load in or collect spots data
-spotsPath = os.path.join(topPath,'spots','spots.npz')
-
-# Get data from spots data  
+spotsPath = os.path.join(topPath_nfs,'spots','spots.npz') 
 spotData = np.load(spotsPath)
-spotInds = np.arange(0,25) 
+K = len(spotData['etas'])
+spotInds = np.arange(0,K) 
 
 # Full dexela image size and roi size
 params = {}
-# params['detector'] = 'eiger'
+
 params['detector'] = 'dexela'
-# Exsitu Yaml
-params['yamlFile'] = '/nfs/chess/user/dbanco/c103_processing/dexelas_calibrated_ruby_0504_v01.yml'
-# params['yamlFile'] = '/mnt/scratch/dbanco/mruby_eiger_calibration_single_grain_v01.yml'
 params['imSize'] = (4888,7300) 
+params['yamlFile'] = '/nfs/chess/user/dbanco/c103_processing/dexelas_calibrated_ruby_0504_v01.yml'
+
+# params['detector'] = 'eiger'
 # params['imSize'] = (5000,5000) 
+# params['yamlFile'] = '/mnt/scratch/dbanco/mruby_eiger_calibration_single_grain_v01.yml'
+
 params['roiSize'] = 40
 params['gamma'] = [8,8,4,4]
 
 scan1 = 1
 scan0 = scan1 - 1 
 # Run on CHESS machine
-initTracksPath = os.path.join(topPath,'initTracks')
-sf.initExsituTracks(initTracksPath,exsituPath, spotData, spotInds, params, scan0)
+sf.initExsituTracks(initTracksPath_mnt,exsituPath, spotData, spotInds, params, scan0)
 
 # # Run on Wrangler
-# Copy track files from init to outputs
+# # Copy track files from init to outputs
 # for k in spotInds:
-#     src = os.path.join(initTracksPath,'trackData_{k}.pkl')
-#     des = os.path.join(topPath,'outputs')
+#     if np.mod(k,1000) == 0:
+#         print(k)
+#     src = os.path.join(initTracksPath_nfs,f'trackData_{k}.pkl')
+#     des = os.path.join(initTracksPath_mnt,f'trackData_{k}.pkl')
+#     # des = os.path.join(topPath_mnt,'outputs',f'trackData_{k}.pkl')
 #     shutil.copyfile(src,des)
 
 # sf.spotTracker(dataPath,topPath,exsituPath,spotData,spotInds,params,scan1)
--- Python/SPOTFETCH/c103_exp_setup_script.py
+++ Python/SPOTFETCH/c103_exp_setup_script.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu May  2 10:07:45 2024
+Created on Thu Jun  6 15:02:29 2024
 
 @author: dpqb1
 """
@@ -8,62 +8,51 @@ Created on Thu May  2 10:07:45 2024
 import numpy as np
 import spotfetch as sf
 import os
-import matplotlib.pyplot as plt
+import shutil
+
+# %% Processing Setup
 
 # Output data path
-# Sets up directories and loads in the spot files
-for i in range(1,3):
-    for j in range(1,3):
-        # outPath = f"/mnt/scratch/dbanco/c103_processing/Sample-{i}"
-        outPath = f"/nfs/chess/user/dbanco/c103_processing/Sample-{i}"
-        outPath2 = os.path.join(outPath,f'layer-{j}')
-        outPath3 = os.path.join(outPath2,'spots')
-        outPath4 = os.path.join(outPath2,'jobs')
-        outPath5 = os.path.join(outPath2,'outputs')
-        outPath6 = os.path.join(outPath2,'initTracks')
-        for path_i in [outPath,outPath2,outPath3,outPath4,outPath5,outPath6]:
-            if not os.path.exists(path_i):
-                os.mkdir(path_i)
-                
-        # Load in or collect spots data
-        topPath = '/nfs/chess/aux/user/wkk32/C103-spots-grains-files'
-        spotsDir = f'Sample-{i}/c103-{i}-reconstruction_grains-layer-{j}'
-        
-        # Get data from spots data
-        # sf.collectSpotsData(outPath3, os.path.join(topPath,spotsDir))
-      
-# %%
-fname = '/mnt/scratch/pagan-dwellfatigue-series-cycle-2024-2/ti6242-sample1-2_0011_EIG16M_CdTe_000001.h5'
-spotsPath = f"/mnt/scratch/dbanco/c103_processing/Sample-1/layer-1/spots"
+topPath_nfs = "/nfs/chess/user/dbanco/c103_processing/Sample-1/layer-1"
+topPath_mnt = "/mnt/scratch/dbanco/c103_processing/Sample-1/layer-1"
 
+# Raw HEXD Data
+exsituPath = "/nfs/chess/raw/2024-2/id3a/miller-3528-c/c103-1-ff-1/1/ff"
+dataPath = "/mnt/scratch/....TBD"
+initTracksPath_nfs = os.path.join(topPath_nfs,'initTracks')
+initTracksPath_mnt = os.path.join(topPath_mnt,'initTracks')
+
+# Load in or collect spots data
+spotsPath = os.path.join(topPath_mnt,'spots','spots.npz') 
+spotData = np.load(spotsPath)
+K = len(spotData['etas'])
+spotInds = np.arange(0,K) 
+
+# Full dexela image size and roi size
 params = {}
-params['detector'] = 'eiger'
-params['yamlFile'] = '/mnt/scratch/dbanco/mruby_eiger_calibration_single_grain_v01.yml'
-params['imSize'] = (5000,5000) 
-params['roiSize'] = 40  
+# params['detector'] = 'eiger'
+params['detector'] = 'dexela'
+# Exsitu Yaml
+params['yamlFile'] = '/nfs/chess/user/dbanco/c103_processing/dexelas_calibrated_ruby_0504_v01.yml'
+# params['yamlFile'] = '/mnt/scratch/dbanco/mruby_eiger_calibration_single_grain_v01.yml'
+params['imSize'] = (4888,7300) 
+# params['imSize'] = (5000,5000) 
+params['roiSize'] = 40
+params['gamma'] = [8,8,4,4]
 
-# Show HEXD frame with spot wedges
-frame = 10
-spotData = np.load(os.path.join(spotsPath,'spots.npz'))
-sf.plotSpotWedges(spotData,fname,fname,frame,params)
+scan1 = 1
+scan0 = scan1 - 1 
+# Run on CHESS machine
+sf.initExsituTracks(initTracksPath_nfs,exsituPath, spotData, spotInds, params, scan0)
 
-# Extract ROIS
-# roi_list = sf.loadSpotsAtFrame(spotData,fname1,fname2,frame,params)
+# # Run on Wrangler
+# # Copy track files from init to outputs
+# for k in spotInds:
+#     if np.mod(k,1000) == 0:
+#         print(k)
+#     src = os.path.join(initTracksPath_nfs,f'trackData_{k}.pkl')
+#     des = os.path.join(initTracksPath_mnt,f'trackData_{k}.pkl')
+#     # des = os.path.join(topPath_mnt,'outputs',f'trackData_{k}.pkl')
+#     shutil.copyfile(src,des)
 
-# Plot ROIs
-# sf.plotROIs(roi_list,num_cols = 5)
-
-# %% Plot range of spots
-# Notes:
-#   ind = 0, frame =4, timeFrms = range(53,68), omeFrms = range(2,7)
-
-# ind = 0
-# tth = spotData['tths'][ind]
-# eta = spotData['etas'][ind]
-# frame = 4
-# timeFrms = range(51,61)
-# omeFrms = range(-2,9)
-
-# sf.roiAdjacent(yamlFile,ind,tth,eta,imSize,roi_size,frame,omeFrms,timeFrms)
-
-            
+# sf.spotTracker(dataPath,topPath,exsituPath,spotData,spotInds,params,scan1)
