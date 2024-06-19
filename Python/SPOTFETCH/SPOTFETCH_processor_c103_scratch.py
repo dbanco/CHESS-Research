e299250d798ef533de925b749c8702977d86c76c
Daniel Banco <dpqb10@gmail.com>
2024-06-19 14:37:39 -0400

    Scratch side changes

--- Python/SPOTFETCH/SPOTFETCH_processor_c103_scratch.py
+++ Python/SPOTFETCH/SPOTFETCH_processor_c103_scratch.py
@@ -13,19 +13,20 @@ import shutil
 # %% Processing Setup
 
 # Output data path
-topPath = "/nfs/chess/user/dbanco/c103_processing/Sample-1/layer-1"
-# topPath = "/mnt/scratch/dbanco/c103_processing/Sample-1/layer-1"
+topPath_nfs = "/nfs/chess/user/dbanco/c103_processing/Sample-1/layer-1"
+topPath_mnt = "/mnt/scratch/dbanco/c103_processing/Sample-1/layer-1"
 
 # Raw HEXD Data
 exsituPath = "/nfs/chess/raw/2024-2/id3a/miller-3528-c/c103-1-ff-1/1/ff"
 dataPath = "/mnt/scratch/....TBD"
+initTracksPath_nfs = os.path.join(topPath_nfs,'initTracks')
+initTracksPath_mnt = os.path.join(topPath_mnt,'initTracks')
 
 # Load in or collect spots data
-spotsPath = os.path.join(topPath,'spots','spots.npz')
-
-# Get data from spots data  
+spotsPath = os.path.join(topPath_mnt,'spots','spots.npz') 
 spotData = np.load(spotsPath)
-spotInds = np.arange(0,25) 
+K = len(spotData['etas'])
+spotInds = np.arange(0,K) 
 
 # Full dexela image size and roi size
 params = {}
@@ -42,14 +43,17 @@ params['gamma'] = [8,8,4,4]
 scan1 = 1
 scan0 = scan1 - 1 
 # Run on CHESS machine
-initTracksPath = os.path.join(topPath,'initTracks')
-sf.initExsituTracks(initTracksPath,exsituPath, spotData, spotInds, params, scan0)
 
-# # Run on Wrangler
+# sf.initExsituTracks(initTracksPath,exsituPath, spotData, spotInds, params, scan0)
+
+# Run on Wrangler
 # Copy track files from init to outputs
-# for k in spotInds:
-#     src = os.path.join(initTracksPath,'trackData_{k}.pkl')
-#     des = os.path.join(topPath,'outputs')
-#     shutil.copyfile(src,des)
+for k in spotInds:
+    if np.mod(k,1000) == 0:
+        print(k)
+    src = os.path.join(initTracksPath_nfs,f'trackData_{k}.pkl')
+    des = os.path.join(initTracksPath_mnt,f'trackData_{k}.pkl')
+    # des = os.path.join(topPath_mnt,'outputs',f'trackData_{k}.pkl')
+    shutil.copyfile(src,des)
 
 # sf.spotTracker(dataPath,topPath,exsituPath,spotData,spotInds,params,scan1)
