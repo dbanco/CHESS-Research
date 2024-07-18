5e3e102ad71825d35f98c997ab23f494689d8c17
Daniel Banco <dpqb10@gmail.com>
2024-07-18 13:59:21 -0400

    chess changes

--- Python/SPOTFETCH/tracker_c103_scratch_Sample-1.py
+++ Python/SPOTFETCH/tracker_c103_scratch_Sample-1.py
@@ -14,7 +14,8 @@ topPath = "/nfs/chess/user/dbanco/c103_processing/Sample-1"
 dataPath = "/nfs/chess/id1a3/2024-2/nygren-4125-a/nygren-series-cycle-2024-2-chessdaq"
 num1 = 2
 num2 = 353
-exsituPath = os.path.join(dataPath,f'c103-1-ungripped-1_{num1:0>4}_EIG16M_CdTe_{num2:0>6}.h5')
+# exsituPath = os.path.join(dataPath,f'c103-1-ungripped-1_{num1:0>4}_EIG16M_CdTe_000353.h5')
+exsituPath = os.path.join(dataPath,"c103-1-ff-1_0004_EIG16M_CdTe_000364.h5")
 dataFile = os.path.join(dataPath,"c103-1-ff-1_*_EIG16M_CdTe_{num2:0>6}.h5")
 
 
@@ -34,7 +35,7 @@ params['yamlFile'] = '/nfs/chess/user/dbanco/c103_processing/eiger16M_monolith_m
 # params['imSize'] = (4888,7300) 
 # params['yamlFile'] = '/nfs/chess/user/dbanco/c103_processing/dexelas_calibrated_ruby_0504_v01.yml'
 params['roiSize'] = [40,40]
-params['gamma'] = [3,5,4,4] #[eta,tth,fwhm_eta,fwhm_tth]
+params['gamma'] = [3,5,4,4] #[eta,tth,fwhm_eta,fwhm_tth]0
 params['pool'] = 16
 
 # %% 4. Inspect spot tracks on ex-situ and initial scan data
@@ -54,12 +55,16 @@ sf.roiTrackVisual(spotInds,spotData,dome,scanRange,trackPath,dataFile,params)
 # sf.plotROIs(roi_list)s
 
 # # %% 6. Begin Processing
+# spotInds = [113]
 # initTracksPath = os.path.join(topPath,'outputs')
-# sf.initExsituTracks(initTracksPath,exsituPath, spotData, spotInds, params, 353)
+sf.initExsituTracks(initTracksPath,exsituPath,spotData, spotInds, params, 364)
+
+# sf.roiTrackVisual([113],spotData,dome,scanRange,trackPath,dataFile,params)
 
 # Sequence: 353, 364, 368, 372, 376, 380
 # num1 = 4
 # num2 = 383
 # advance = False
 # for num2 in scanRange:
-#     sf.spotTracker(dataFile,topPath,spotData,spotInds,params,num1,num2,advance)
+#     # num2 = 353
+    sf.spotTracker(dataFile,topPath,spotData,spotInds,params,num1,num2,advance)
