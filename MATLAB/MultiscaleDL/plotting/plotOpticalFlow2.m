32217bbddd842b7b4129e1410c858dfc54439baa
dbanco <dpqb10@gmail.com>
2024-08-01 12:38:12 -0700

    adding more to git

---
+++ MATLAB/MultiscaleDL/plotting/plotOpticalFlow2.m
@@ -0,0 +1,94 @@
+function plotOpticalFlow2(X,K,opt,fName,outputDir)
+%UNTITLED2 Summary of this function goes here
+%   Detailed explanation goes here
+if nargin > 3
+    gifDir = outputDir;
+    outFile = fullfile(gifDir,fName);
+end
+X = squeeze(X);
+[N,KJ,T] = size(X);
+J = KJ/K;
+% Compute opticalflow
+[u,v] = computeHornSchunkDict(X,K,opt.Smoothness,opt.HSiters);
+[u2,v2] = computeHornSchunkDictLS(X,K,opt.Smoothness,opt.HSiters);
+% Show velocity in time
+% avgU = mean(mean(u,1),2);
+% avgV = mean(mean(v,1),2);
+% timeU = zeros(N,KJ);
+% timeV = zeros(N,KJ);
+% % Find max of velocity
+% figure
+% for t = 1:100
+%     [valU,ui] = max(u(:,:,t),[],'all');
+%     [valV,vi] = max(v(:,:,t),[],'all');
+%     timeU(ui) = timeU(ui) + valU;
+%     timeV(ui) = timeV(ui) + valV;
+% 
+% end
+% quiver(timeV',timeU')
+% hold on
+% Compute optical flow constraint
+% OF = sum( (opticalFlowOp(X,u,v,K)).^2,'all')
+
+window = 50:100;
+
+% View optical flow
+fig = figure;
+fig.Position = [1 1 1.6452e+03 554.8000];
+opticFlow1 = opticalFlowHS;
+opticFlow2 = opticalFlowFarneback;
+for t = 1:T
+    frameGray = squeeze(X(:,1:J,t));
+    framePlot = frameGray(window,:)';
+
+    flow1 = estimateFlow(opticFlow1,frameGray);
+    flow2 = estimateFlow(opticFlow2,frameGray);
+
+    subplot(4,1,1)
+    imagesc(framePlot)
+    hold on
+    quiver(v(window,1:J,t)',u(window,1:J,t)')
+    q = findobj(gca,'type','Quiver');
+    q.Color = 'w';
+    hold off
+
+    subplot(4,1,2)
+    imagesc(framePlot)
+    hold on
+    quiver(flow1.Vy(window,:)',flow1.Vx(window,:)')
+    q = findobj(gca,'type','Quiver');
+    q.Color = 'w';
+    hold off
+
+    subplot(4,1,3)
+    imagesc(framePlot)
+    hold on
+    quiver(flow2.Vy(window,:)',flow2.Vx(window,:)')
+    q = findobj(gca,'type','Quiver');
+    q.Color = 'w';
+    hold off
+
+    subplot(4,1,4)
+    imagesc(framePlot)
+    hold on
+    quiver(v2(window,1:J,t)',u2(window,1:J,t)')
+    q = findobj(gca,'type','Quiver');
+    q.Color = 'w';
+    hold off
+
+
+    % Save the image to a GIF file
+    if nargin > 3
+        frame = getframe(fig);
+        im = frame2im(frame);
+        [imind,cm] = rgb2ind(im,256);
+        if t == 1
+            imwrite(imind,cm,outFile,'gif','Loopcount',inf,'DelayTime',0.2);
+        else
+            imwrite(imind,cm,outFile,'gif','WriteMode','append','DelayTime',0.2);
+        end
+    end
+    pause()
+end
+
+end
