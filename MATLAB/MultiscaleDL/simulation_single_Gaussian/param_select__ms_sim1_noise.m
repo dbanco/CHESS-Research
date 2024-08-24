83b109a0685c4fd5581c92d2f607fad3364f4ff3
dbanco <dpqb10@gmail.com>
2024-08-24 16:42:35 -0700

    Add more values for parameter selection script

--- MATLAB/MultiscaleDL/simulation_single_Gaussian/param_select__ms_sim1_noise.m
+++ MATLAB/MultiscaleDL/simulation_single_Gaussian/param_select__ms_sim1_noise.m
@@ -9,7 +9,6 @@ for n = 2:NN
     
     files = dir(fullfile(folderPath, '*.mat'));
     
-    
     % Extract the file names and store them in a cell array
     matFileNames = {files.name};
     
@@ -56,16 +55,98 @@ for n = 2:NN
 %     criterion = abs(err_sort)/max(err_sort) + abs(l1_sort)/max(l1_sort);
 %     criterion = abs((err_sort-min(err_sort))/max(err_sort-min(err_sort))) +...
 %                 abs((l1_sort-min(l1_sort))/max(l1_sort-min(l1_sort)));
-    criterion = abs((diff(err_sort,1)./diff(l1_sort,1)) + 0.04);
+%     criterion = abs((diff(err_sort,1)./diff(l1_sort,1)) + 0.04);
+%     criterion = abs((diff(err_sort,2)./diff(l1_sort,2)));
+%     if n == 2
+    dx = diff(l1_sort,1);
+    ddx = diff(l1_sort,2);
+    dy = diff(err_sort,1);
+    ddy = diff(err_sort,2);
+    curvature = abs(dx(2:end).*ddy - dy(2:end).*ddx)./(dx(2:end).^2 + dy(2:end).^2).^1.5;
+    criterion = curvature;
+    criterion(1:3) = 0; 
+    criterion(end-2:end)=0;
 
-    [minCrit, selInd] = min(criterion);
-    
+    [minCrit, selInd] = max(criterion);
+    selInd = selInd + (numel(err_sort)-numel(criterion))/2;
+    l1_sel = l1_sort(selInd);
+%     else
+%         criterion = abs(l1_sort-l1_sel);
+%         [minCrit, selInd] = min(criterion);
+%     end
+
     hold on
     plot(l1_sort(selInd),err_sort(selInd),'sr','MarkerSize',10);
     saveas(fig_ps,fullfile(folderPath,'param_select.png'));
     % Get Lambda for chosen parameter
     load(fullfile(folderPath,matFileNames{ind(selInd)}))
     selected_lam(n) = outputs.lambda;
+% 
+%     figure
+%     plot(curvature)
+
 end
 selected_lam
+
+%% Next plot the true recovery as a function of SNR
+sigmas = 0:0.01:0.1;
+meanSNR = zeros(numel(sigmas),1);
+for n = 2:numel(sigmas)
+    [y,y_true,N,M,T] = gaus_example_multiscale_dl(sigmas(n));
+    
+    SNR = zeros(T,1);
+    nPwr = zeros(T,1);
+    sigPwr = zeros(T,1);
+    for t = 1:T
+        SNR(t) = norm(y_true(:,t))/norm(y(:,t)-y_true(:,t));
+        nPwr(t) = norm(y(:,t)-y_true(:,t));
+        sigPwr(t) = norm(y_true(:,t));
+    end
+    
+    meanSNR(n) = mean(SNR);
+end
+
+NN = numel(sigmas);
+trueErr = zeros(NN,1);
+dataErr = zeros(NN,1);
+noiseNorm = zeros(NN,1);
+noiseNorm2 = zeros(NN,1);
+outDir = "C:\Users\dpqb1\Documents\Outputs2024_8_23\";
+
+for n = 2:NN
+    gausDir = "gaus_example_8_23_24_X0_D0_V00_sig_"+num2str(n);
+    dataFile = sprintf("output_j1_sig_%1.2s_lam1_%1.2s_lam2_0.00e+00",sigmas(n),selected_lam(n));
+    [y,y_true,N,M,T] = gaus_example_multiscale_dl(sigmas(n));
+    load(fullfile(outDir,gausDir,dataFile))
+    D = outputs.D;
+    N = outputs.N;
+    M = outputs.M;
+    y = outputs.y;
+    X = outputs.X;
+    scales = outputs.scales;
+    center = (M+1)/2;
+    
+    AD = reSampleCustomArrayCenter(N,D,scales,center);
+    AD = padarray(AD,[0 M-1 0],0,'post');
+    ADf = fft2(AD);
+    Yhat = unpad(squeeze(ifft2(sum(bsxfun(@times,ADf,fft2(X)),3),'symmetric')),M-1,'pre');
+    Yhat = gather(Yhat);
+
+    trueErr(n) = norm(y_true-Yhat);
+    dataErr(n) = norm(squeeze(outputs.y)-Yhat);
+    noiseNorm(n) = norm(randn(N,T)*sigmas(n));
+    noiseNorm2(n) = norm(y_true-squeeze(outputs.y));
+end
+
+figure()
+hold on
+plot(meanSNR(2:NN),trueErr(2:NN),'o-')
+plot(meanSNR(2:NN),dataErr(2:NN),'x-')
+plot(meanSNR(2:NN),noiseNorm(2:NN),'s-')
+plot(meanSNR(2:NN),noiseNorm2(2:NN),'x-')
+xlabel('SNR','Fontsize',14)
+ylabel('Error','Fontsize',14)
+legend('$\|\hat{{\bf b}}-{\bf f}\|_2$','$\|\hat{{\bf b}}-{\bf b}\|_2$',...
+    '$\|\mathcal{N}(0,s)\|_2$','$\|{\bf b}-{\bf f}\|_2$',...
+    'interpreter','latex','Fontsize',14)
 
