97019b36f340f15fd77492bc839d39c837d7b54d
dbanco <dpqb10@gmail.com>
2025-01-16 22:28:40 -0800

    trials for of mcdl-of sim1

---
+++ MATLAB/MultiscaleDL/sim1_single_Gaussian/gauss_example_switch_slurm_trials_of.m
@@ -0,0 +1,114 @@
+%% Multiscale 1D dictionary learning toy problem
+% Directory
+lambdaVals = [1e-4,5e-4,1e-3,5e-3,1e-2,2e-2,linspace(3e-2,8e-1,100)];
+lambdaHSVals = [0 1e-4 5e-4 1e-3 2e-3];
+lambdaOFVals = [0 1e-4,5e-4,1e-3,5e-3,1e-2,linspace(5e-2,1,50)];
+
+% Experiment Setup
+sigmas = 0:0.01:0.1;
+
+% Set up algorithm parameters
+opt.plotDict = 0;
+opt.Verbose = 1;
+opt.MaxMainIter = 1000;
+opt.MaxCGIter = 100;
+opt.CGTol = 1e-6;
+opt.MaxCGIterX = 100;
+opt.CGTolX = 1e-6;
+% Rho and sigma params
+opt.rho = 300;
+opt.sigma = 50;
+opt.AutoRho = 1;
+opt.AutoRhoPeriod = 1;
+opt.AutoSigma = 1;
+opt.AutoSigmaPeriod = 1;
+opt.XRelaxParam = 1.8;
+opt.DRelaxParam = 1.8;
+opt.NonNegCoef = 1;
+opt.NonnegativeDict = 1;
+opt.UpdateVelocity = 1;
+opt.HSiters = 100;
+opt.useGpu = 0;
+opt.Xfixed = 0;
+opt.Dfixed = 0;
+opt.Penalty = 'l1-norm';
+% opt.Penalty = 'log';
+opt.coefInit = 'zeros';
+opt.dictInit = 'flat';
+opt.a = 1;
+
+penalties = {'l1-norm','log'};
+xinits = {'zeros','true'};
+dinits = {'flat','true'};
+dfixes = {0,1};
+
+load('/cluster/home/dbanco02/CHESS-Research/MATLAB/MultiscaleDL/sim1_single_Gaussian/sim1_selected_lam_s.mat')
+
+scriptFileName = 'mcdlof_bash.sh';
+funcName = 'mcdlof_wrapper_sim1_switch';
+jobDir = '/cluster/home/dbanco02/jobs/';
+k = 1;
+
+for r_ind = 1:25
+for s1 = 1
+    for s2 = 2
+        for s3 = 1
+            for s4 = 1
+
+opt.Dfixed = dfixes{s1};
+opt.Penalty = penalties{s2};
+opt.coefInit = xinits{s3};
+opt.dictInit = dinits{s4};
+if (opt.Dfixed == 1) && strcmp(opt.dictInit, 'flat')
+    continue
+end
+for sig_i = 2:9
+    j_s_select = find(lambdaVals == selected_lam_s_vec(sig_i));
+    J_of_select = find(lambdaOFVals == selected_lam_of_vec(sig_i));
+    for j_s = j_s_select
+        for j_hs = 1
+            for j_of = 1
+                dataset = 'steps_matched';
+                topDir = ['/cluster/home/dbanco02/Outputs',num2str(r_ind),...
+                    '_D',opt.dictInit,num2str(opt.Dfixed),...
+                    '_X',opt.coefInit,num2str(opt.Xfixed),'/',...
+                    dataset,'_',opt.Penalty,'_results'];
+                
+                varin = {lambdaVals,lambdaOFVals,lambdaHSVals,...
+                        j_s,j_of,j_hs,sigmas,sig_i,opt,topDir,dataset};
+                save(fullfile(jobDir,['varin_',num2str(k),'.mat']),'varin','funcName')
+                k = k + 1;
+            end
+        end
+    end
+end
+            end
+        end
+    end
+end
+end
+
+slurm_write_bash(k-1,jobDir,scriptFileName,sprintf('1-%i',k-1))
+%% Compare data
+%
+% [yn1,y_m,N,M,T,~,~] = gaus_example_matched_multiscale_dl(0);
+% [yn2,y_um,N,M,T,~,~] = gaus_example_unmatched_multiscale_dl(0);
+% figure(2)
+% subplot(3,1,1)
+% imagesc(y_m)
+% clim([0,0.4])
+% colorbar()
+% 
+% subplot(3,1,2)
+% imagesc(y_um)
+% clim([0,0.4])
+% colorbar()
+% 
+% subplot(3,1,3)
+% imagesc(abs(y_m -y_um))
+% colorbar()
+% 
+% figure(3)
+% plot(y_um(:,10))
+% hold on
+% plot(y_m(:,10))
---
+++ MATLAB/MultiscaleDL/sim1_single_Gaussian/param_select_ms_sim1_of.m
@@ -0,0 +1,86 @@
+lambdaVals = [1e-4,5e-4,1e-3,5e-3,1e-2,2e-2,linspace(3e-2,8e-1,100)];
+lambdaHSVals = [0 1e-4 5e-4 1e-3 2e-3];
+lambdaOFVals = [0 1e-4,5e-4,1e-3,5e-3,1e-2,linspace(5e-2,1,50)];
+sigmas = 0:0.01:0.1;
+NN = numel(sigmas);
+
+tradeoff_s = 0.4;
+tradeoff_of = 1;
+scaleP = [0.4,5.4,9.48,116,0,100];
+
+% criterion = 'discrepancy';
+criterion = 'truth_error';
+
+selected_lam_s_vec = zeros(NN,1);
+selected_lam_of_vec = zeros(NN,1);
+
+% topDir = 'C:\Users\dpqb1\Documents\Outputs2024_10_10_Dtrue_Xtrue';
+% topDir = 'C:\Users\dpqb1\Documents\Outputs2024_10_31_Dtrue1_Xzeros0';
+% dirStartS = 'steps_matched_results';
+fig_num = 32;
+for hs_ind = 3
+    dataset = 'steps_matched';
+    testType = 'Dflat0_Xzeros0';
+    topDir = ['E:\Outputs_lam_of',int2str(hs_ind),'_',testType];
+    dirStartS = 'steps_matched_log_results';
+    
+    for n = 2:9
+        inDir = [topDir,'\',dirStartS,'_sig_',num2str(n)];
+         % True error
+        [~,y_true,~,~,~] = gaus_example_switch_multiscale_dl(sigmas(n),dataset);
+    
+        [lambda_of_sel,j_of] = param_select_lambda_of(inDir,tradeoff_s,scaleP,fig_num,criterion,sigmas(n),y_true);
+        selected_lam_of_vec(n) = lambda_of_sel;
+    end
+end
+    LcurveFile = fullfile(topDir,['l-curve_plot',dirStartS,'.png']); 
+    fig = gcf;
+    fig.Position = [100 100 1400 800];
+    saveas(gcf, LcurveFile);
+    removeWhiteSpace(LcurveFile)
+
+%% Next plot the true recovery as a function of SNR
+sigmas = 0:0.01:0.1;
+meanSNR = zeros(numel(sigmas),1);
+for n = 1:NN
+    [y,y_true,N,M,T] = gaus_example_switch_multiscale_dl(sigmas(n),dataset);
+    SNR = zeros(T,1);
+    nPwr = zeros(T,1);
+    sigPwr = zeros(T,1);
+    for t = 1:T
+        SNR(t) = norm(y_true(:,t))/norm(y(:,t)-y_true(:,t));
+        nPwr(t) = norm(y(:,t)-y_true(:,t));
+        sigPwr(t) = norm(y_true(:,t));
+    end
+    
+    meanSNR(n) = mean(SNR);
+end
+%% Need to write a function to compute the various errors for all noise levels
+% and that maybe just takes the file name as input so that it works on both
+[noiseNorm,trueErrS,dataErrS,~,~] = simError(y_true,sigmas,topDir,dirStartS,selected_lam_s_vec,lambdaVals);
+
+%     [noiseNorm,trueErrS,dataErrS,trueErrOF,dataErrOF] = simError(y_true,sigmas,outDirOF,dirStartS,...
+%                                 selected_lam_s_vec,dirStartOF,selected_lam_of_vec,lambdaOFVals);
+    
+%%
+figure()
+hold on
+plot(meanSNR,noiseNorm,'s-')
+plot(meanSNR,trueErrS,'o-')
+xlabel('SNR','Fontsize',14)
+ylabel('Error','Fontsize',14)
+legend('$\|{\bf w}\|_2$','$\|\hat{{\bf b}}-{\bf f}\|_2$',...ub hbh h                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        
+    '$\|\hat{{\bf b}}-{\bf f}\|_2$ (OF)',...
+    'interpreter','latex','Fontsize',14)
+
+
+%% Next copy figures associated with selected parameters to a folder
+% pptFile = 'C:\Users\dpqb1\Documents\MCDL Paper\recons_dicts_sim1_hs_2.pptx';
+% titleStr = 'Sim 1 Recovery';
+% createPowerpointSim(pptFile,titleStr,meanSNR,topDir,sigmas,dirStartS,lambdaVals,dirStartOF,...
+%     lambdaOFVals,lambdaHSVals,selected_lam_of_vec,selected_lam_s_vec)
+
+pptFile = ['C:\Users\dpqb1\Documents\MCDL Paper\recons_12_12_',criterion,'_',testType,'_',dirStartS,'.pptx'];
+titleStr = ['Sim 1 Recovery, ',testType,', ',dirStartS];
+createPowerpointSimS(pptFile,titleStr,meanSNR,topDir,sigmas,dirStartS,selected_lam_s_vec,lambdaVals,LcurveFile,criterion)
+
