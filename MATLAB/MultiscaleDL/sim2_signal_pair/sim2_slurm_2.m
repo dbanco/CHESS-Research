5a8af17933340ae10c533020f50ae4931d5f68be
dbanco <dpqb10@gmail.com>
2025-01-20 17:32:47 -0800

    Preparing for sim2

--- MATLAB/MultiscaleDL/sim2_signal_pair/sim2_slurm_2.m
+++ MATLAB/MultiscaleDL/sim2_signal_pair/sim2_slurm_2.m
@@ -80,10 +80,20 @@ for s4 = 1
         '_X',opt.coefInit,num2str(opt.Xfixed),'/',...
         dataset,'_',opt.Penalty,'_results'];
     
-    varin = setupVarinMCDLOF(opt,topDir,dataset,K,scales,sigmas,sig_ind,...
-        lambdaVals,lambdaOFVals,lambdaHSVals,ind1,ind2,ind3);
-    save(fullfile(jobDir,['varin_',num2str(k),'.mat']),'varin','funcName')
-    k = k + 1;
+    for sig_i = sig_ind
+        % j_s_select = find(lambdaVals == selected_lam_s_vec(sig_i));
+        for j_s = ind1
+            for j_hs = ind2
+                for j_of = ind3
+                    varin = {lambdaVals,lambdaOFVals,lambdaHSVals,...
+                            j_s,j_of,j_hs,sigmas,sig_i,opt,topDir,dataset,K,scales};
+                    save(fullfile(jobDir,['varin_',num2str(k),'.mat']),'varin','funcName')
+                    k = k + 1;
+                end
+            end
+        end
+    end
+    
 end
 end
 end
