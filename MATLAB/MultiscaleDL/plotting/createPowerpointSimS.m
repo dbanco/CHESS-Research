16813073eaf99090dc9f460621578bb757f0ccd4
dbanco <dpqb10@gmail.com>
2025-01-20 17:06:01 -0800

    Preparing for sim2

--- MATLAB/MultiscaleDL/plotting/createPowerpointSimS.m
+++ MATLAB/MultiscaleDL/plotting/createPowerpointSimS.m
@@ -20,7 +20,7 @@ img = Picture(LcurveFile);
 replace(slide2, 'Content', img);
 
 NN = numel(sigmas);
-for n = 2:9
+for n = 2:4
     spDir = [dirStartS,'_sig_',num2str(n)];
 
     j_s = find(lambdaVals == selected_lam_s_vec(n));
