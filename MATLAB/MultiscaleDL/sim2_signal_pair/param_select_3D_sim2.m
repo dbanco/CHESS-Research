0ae6d1155d671b57f21b564ba7e22eec431f54e6
dbanco <dpqb10@gmail.com>
2025-02-06 22:24:59 -0800

    Slurm script to run sim2, lam_of,hs parameter selection

--- MATLAB/MultiscaleDL/sim2_signal_pair/param_select_3D_sim2.m
+++ MATLAB/MultiscaleDL/sim2_signal_pair/param_select_3D_sim2.m
@@ -4,31 +4,53 @@ lambdaOFVals = [0 1e-4,5e-4,1e-3,5e-3,1e-2,linspace(5e-2,1,50)];
 sigmas = 0:0.01:0.1;
 NN = numel(sigmas);
 
-criterion = 'discrepancy';
-% criterion = 'truth_error';
-
-% topDir = 'C:\Users\dpqb1\Documents\Outputs2024_10_10_Dtrue_Xtrue';
-% topDir = 'C:\Users\dpqb1\Documents\Outputs2024_10_31_Dtrue1_Xzeros0';
-% dirStartS = 'steps_matched_results';
 fig_num = 22;
 
-dataset = 'dissertation';
-testType = 'Dflat0_Xzeros0';
-topDir = ['E:\MCDLOF_processing\Outputs_',dataset,'_',testType];
-dirStartS = [dataset,'_log_results'];
+datasets = {'sim2_gaussian_tooth_matched','sim2_gaussian_tooth_unmatched',...
+            'sim2_gaussian_tooth_matched2','sim2_gaussian_tooth_unmatched2',...
+            'dissertation','dissertation_long','dissertation_long_separate'};
+penalties = {'l1-norm','log'};
+xinits = {'zeros','true'};
+dinits = {'rand','flat','true'};
+dfixes = {0,1};
+recenters = {0,1};
+
+% Setup Dataset
+s0 = 6;
+s1 = 2;
+s2 = 1;
+s3 = 2;
+s4 = 1;
+s5 = 2;
+dataset = datasets{s0};
+opt.Penalty = penalties{s1};
+opt.coefInit = xinits{s2};
+opt.dictInit = dinits{s3};
+opt.Dfixed = dfixes{s4};
+opt.Recenter = recenters{s5};
+opt.Xfixed = 0;
+
+suffix = [dataset,'_',opt.Penalty,...
+        '_D',opt.dictInit,num2str(opt.Dfixed),...
+        '_X',opt.coefInit,num2str(opt.Xfixed),...
+        '_recenter',num2str(opt.Recenter)];
+
+topDir = ['E:\MCDLOF_processing\Outputs_',suffix];
+
+% criterion = 'discrepancy';
+criterion = 'truth_error';
 
 selected_lam_all_vec = zeros(NN,3);
 selected_inds = zeros(NN,1);
 sig_ind = 2:4;
 for n = sig_ind
-    inDir = [topDir,'\',dirStartS,'_sig_',num2str(n)];
-     % True error
+    inDir = [topDir,'\results','_sig_',num2str(n)];
     [~,y_true,~,~,~] = sim_switch_multiscale_dl(sigmas(n),dataset);
     [lambda_all,selInd,objective] = param_select_3D(inDir,fig_num,criterion,sigmas(n),y_true);
     selected_lam_all_vec(n,:) = lambda_all;
     selected_inds(n) = selInd;
 end
-LcurveFile = fullfile(topDir,['l-curve_plot',dirStartS,'.png']); 
+LcurveFile = fullfile(topDir,'l-curve_plot.png'); 
 fig = gcf;
 fig.Position = [100 100 1400 800];
 saveas(gcf, LcurveFile);
@@ -49,8 +71,8 @@ legend('$\|{\bf w}\|_2$','$\|\hat{{\bf b}}-{\bf f}\|_2$',...ub hbh h
 
 
 %% Next copy figures associated with selected parameters to a folder
-pptFile = ['C:\Users\dpqb1\Documents\MCDL Paper\sim2_lam_s',criterion,'_',testType,'_',dirStartS,'.pptx'];
-titleStr = ['Sim 2 Recovery, ',testType,', ',dirStartS];
+pptFile = ['C:\Users\dpqb1\Documents\MCDL Paper\sim2_lam_s_',criterion,'_',suffix,'.pptx'];
+titleStr = ['Sim 2 Recovery, ',suffix];
 createPowerpointSimAll(pptFile,titleStr,meanSNR,topDir,dirStartS,sigmas,...
     selected_lam_all_vec,lambdaVals,lambdaOFVals,lambdaHSVals,LcurveFile,criterion,sig_ind)
 
