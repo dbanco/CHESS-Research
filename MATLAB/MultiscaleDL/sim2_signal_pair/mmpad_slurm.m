88453e44f72eb4e4c0f17e659706c712f6a4c364
dbanco <dpqb10@gmail.com>
2025-05-09 08:11:21 -0700

    Update mcdl with mmpad slurm script

--- MATLAB/MultiscaleDL/sim2_signal_pair/mmpad_slurm.m
+++ MATLAB/MultiscaleDL/sim2_signal_pair/mmpad_slurm.m
@@ -1,7 +1,7 @@
 %% Multiscale 1D dictionary learning toy problem
 % Directory
 % lambdaVals = [1e-4,5e-4,1e-3,5e-3,1e-2,2e-2,linspace(3e-2,8e-1,100)];
-lambdaVals = logspace(-3,1,100);
+lambdaVals = logspace(-3,1,150);
 lambdaOFVals = [0 1e-4,5e-4,1e-3,5e-3,1e-2,linspace(5e-2,1,50)];
 lambdaHSVals = [0 1e-4 5e-4 1e-3 2e-3 5e-3];
 
